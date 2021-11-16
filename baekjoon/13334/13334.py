724ff4febd9068a0d97b681d09faa7af713c8fd5
dkkim0122 <dkkim0122@gmail.com>
2021-11-17 00:10:08 +0900

    * 13334 solved. 우선순위 큐는 정렬보다는 최대최소를 찾는 데 쓰이는 것이 좋다.

--- baekjoon/13334/13334.py
+++ baekjoon/13334/13334.py
@@ -1,40 +1,40 @@
-import sys
 import heapq
-
-def search(start, end):
-    max_count = 0
-    mid = (start + end) // 2
-
-    pl = mid - d
-    pr = mid + d
-    print(f'mid = {mid}, pl : {pl}, pr = {pr}')
-
-    if start == end:
-        return 0
-
-    for i in range(n):
-        if points[i][0] >= pl and points[i][1] <= pr:
-            max_count += 1
-
-    max_count = max(max_count, search(start, mid), search(mid, end))
-
-    print(f"max = {max_count}")
-
-    return max_count
+import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-points = []
-
-for i in range(n):
-    point = list(map(int, input().split()))
-    if point[1] < point[0]:
-        point = [point[1], point[0]]
-    heapq.heappush(points, point)
-
-print(points)
+n = int(input().strip())
+line_list = []
+for _ in range(n):
+    line = list(map(int, input().split()))
+    line_list.append(line)
 
 d = int(input())
 
-print(search(points[0][0], points[-1][1]))
+# 후보군을 뽑자
+target_line = []  # 후보군 라인
+
+for line in line_list:
+    start, end = line
+    if abs(start - end) <= d:   # 길이가 기준보다 작은 애들만
+        line = sorted(line)   # 시작점과 끝점을 정렬한다.
+        target_line.append(line)
+
+target_line.sort(key= lambda x:x[1])  # 끝점을 기준으로 오름차순으로 정렬한다.
+
+# 가능한 실제 선분들을 뽑자
+possible_line = []
+ans = 0
+
+for line in target_line:
+    if not possible_line:   # 힙이 비어있으면 일단 해당 후보군을 넣자(어차피 가능한 친구다)
+        heapq.heappush(possible_line, line)
+    else:
+        while possible_line[0][0] < line[1] - d: 
+            heapq.heappop(possible_line)
+            if not possible_line:
+                break
+        heapq.heappush(possible_line, line)
+    ans = max(ans, len(possible_line))
+
+print(ans)
