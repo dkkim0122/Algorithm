4e93e3707102b7415381df282c3305f157971f57
dkkim0122 <dkkim0122@gmail.com>
2021-11-19 22:48:14 +0900

    11724 solved. using dictionary to contain edges

---
+++ baekjoon/11724/11724.py
@@ -0,0 +1,36 @@
+import collections
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+num_node, num_edge = map(int, input().split())
+graph = collections.defaultdict(list)
+
+for i in range(num_edge):
+    node1, node2 = map(int, input().split())
+
+    graph[node1].append(node2)
+    graph[node2].append(node1)
+
+def dfs(graph, start_node):
+    visited = []
+    need_visited = deque()
+    need_visited.append(start_node)
+
+    while need_visited:
+        node = need_visited.popleft()
+        if node not in visited:
+            visited.append(node)
+            need_visited.extend(graph[node])
+    
+    return visited
+
+count = 1
+result = dfs(graph, next(iter(graph)))
+for i in range(1, num_node+1):
+    if i not in result:
+        result.extend(dfs(graph, i))
+        count += 1
+
+print(count)
