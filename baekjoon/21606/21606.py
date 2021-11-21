aa3c367a7846bec7f73481c4736e22df6630a432
dkkim0122 <dkkim0122@gmail.com>
2021-11-21 14:40:03 +0900

    21606 98 points. amended my own 3 points answer

--- baekjoon/21606/21606.py
+++ baekjoon/21606/21606.py
@@ -2,24 +2,27 @@ import sys
 from collections import deque
 sys.setrecursionlimit(10**5)
 
-
 input = sys.stdin.readline
 
-def dfs(start, visited = []):
-      # 각 dfs마다 visited를 따로 돌린다 -> 모든 노드 시작점에 대해 탐색을 각각 해야 하므로
+def dfs(start):
+    visited = []
     visited.append(start)
+    need_visit = deque([start])
 
-    for adj in graph[start]:
-        if adj not in visited: # 방문한 적 없다.
-            if in_out[adj] == 1:  # 방문한 적 없고, 실외면 끝낸다.
+    while need_visit:
+        node = need_visit.pop()
+        for adj in graph[node]:
+            if adj not in visited:
                 visited.append(adj)
-                path.append(visited)
-            else:
-                dfs(adj, visited) # 실내면 거기서부터 다시 실외로 가는 방법 찾기
+                if in_out[adj] == 0:
+                    need_visit.append(adj)
+                elif in_out[adj] == 1:
+                    path.append(visited)
+                    # adj 하나에서 return까지 가면 다른 adj는 못 본다.
 
 
 n = int(input())
-in_out = [0] # 0번째 인덱스는 무시
+in_out = [0]
 graph = [[] for _ in range(n+1)]
 
 for i in input().strip():
@@ -33,7 +36,7 @@ for i in range(n-1):
 path = []
 
 for i in range(1, n+1):
-    if in_out[i] == 1:  # 실외일 때만 실행한다.
-        dfs(i, [])
+    if in_out[i] == 1:
+        dfs(i)
 
 print(len(path))
