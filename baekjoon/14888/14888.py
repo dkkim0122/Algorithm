828023ad19925638d3352842179880d5b500fc88
dkkim0122 <dkkim0122@gmail.com>
2021-11-21 16:23:26 +0900

    14888 solved. using permutations

--- baekjoon/14888/14888.py
+++ baekjoon/14888/14888.py
@@ -1,6 +1,26 @@
 import sys
 from itertools import permutations
+import copy
+
 input= sys.stdin.readline
+
+def calculate(cal_perm):
+    num = copy.deepcopy(nums) # 각 계산마다 nums를 수정하면 안 되므로 deepcopy
+    for i in range(n-1):
+        if cal_perm[i] == '+':
+            num[i+1] = num[i]+num[i+1]
+        elif cal_perm[i] =='-':
+            num[i+1] = num[i]-num[i+1]
+        elif cal_perm[i] =='*':
+            num[i+1] = num[i]*num[i+1]
+        elif cal_perm[i] == '//':
+            if num[i]<0:
+                num[i+1] = -((-num[i])//num[i+1]) # 문제 나눗셈 규정에 맞춰서
+            else:
+                num[i+1] = num[i]//num[i+1]
+    
+    return num[n-1]
+
 
 n = int(input().strip())
 nums = list(map(int, input().split()))
@@ -9,32 +29,12 @@ cal_lists = ['+','-','*','//']
 cal = []
 for i in range(len(lst_lists)):
     for j in range(lst_lists[i]):
-        cal.append(cal_lists[i])
+        cal.append(cal_lists[i]) # 해당 입력받은 연산자들을 순서대로 나열
 
-print(nums)
-print(lst_lists)
-print(cal)
-
-def calculate(cal_perm):
-    for i in range(n-1):
-        if cal_perm[i] == '+':
-            nums[i+1] = nums[i]+nums[i+1]
-        elif cal_perm[i] =='-':
-            nums[i+1] = nums[i]-nums[i+1]
-        elif cal_perm[i] =='*':
-            nums[i+1] = nums[i]*nums[i+1]
-        elif cal_perm[i] == '//':
-            if nums[i]<0:
-                nums[i+1] = -((-nums[i])//nums[i+1])
-            else:
-                nums[i+1] = nums[i]//nums[i+1]
-    
-    return nums[n-1]
-
-
-perm_list = list(permutations(cal))
+perm_list = list(permutations(cal)) # 가능한 연산자 순열을 나열
 result = []
 
+# print(calculate(['+','+','-','*','//']))
 
 for perm in perm_list:
     result.append(calculate(perm))
