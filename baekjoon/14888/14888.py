9673eb2060f764f6781053b52c140c4958e2a329
dkkim0122 <dkkim0122@gmail.com>
2021-11-21 18:30:24 +0900

    14888 solved, using recursion

--- baekjoon/14888/14888.py
+++ baekjoon/14888/14888.py
@@ -1,44 +1,33 @@
 import sys
-from itertools import permutations
-import copy
 
 input= sys.stdin.readline
 
-def calculate(cal_perm):
-    num = copy.deepcopy(nums) # 각 계산마다 nums를 수정하면 안 되므로 deepcopy
-    for i in range(n-1):
-        if cal_perm[i] == '+':
-            num[i+1] = num[i]+num[i+1]
-        elif cal_perm[i] =='-':
-            num[i+1] = num[i]-num[i+1]
-        elif cal_perm[i] =='*':
-            num[i+1] = num[i]*num[i+1]
-        elif cal_perm[i] == '//':
-            if num[i]<0:
-                num[i+1] = -((-num[i])//num[i+1]) # 문제 나눗셈 규정에 맞춰서
-            else:
-                num[i+1] = num[i]//num[i+1]
+def dfs(idx_op, total, plus, minus, multi, divide):
+    if idx_op == n:
+        result.append(total)
+        return 
     
-    return num[n-1]
+    if plus:
+        dfs(idx_op+1, total + nums[idx_op], plus-1, minus, multi, divide)
+    if minus:
+        dfs(idx_op+1, total - nums[idx_op], plus, minus-1, multi, divide)
+    if multi:
+        dfs(idx_op+1, total * nums[idx_op], plus, minus, multi-1, divide)
+    if divide:
+        dfs(idx_op+1, total / nums[idx_op], plus, minus, multi, divide-1)
+
+    return
 
 
 n = int(input().strip())
 nums = list(map(int, input().split()))
-lst_lists = list(map(int, input().split()))
-cal_lists = ['+','-','*','//']
-cal = []
-for i in range(len(lst_lists)):
-    for j in range(lst_lists[i]):
-        cal.append(cal_lists[i]) # 해당 입력받은 연산자들을 순서대로 나열
+cal = list(map(int, input().split()))
 
-perm_list = list(permutations(cal)) # 가능한 연산자 순열을 나열
+print(nums)
+print(cal)
+
 result = []
 
-# print(calculate(['+','+','-','*','//']))
-
-for perm in perm_list:
-    result.append(calculate(perm))
-
+dfs(1, nums[0], cal[0], cal[1], cal[2], cal[3])
 print(max(result))
 print(min(result))
-                
