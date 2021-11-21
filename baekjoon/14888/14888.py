17f6b25a1c28a6aaf49544636d9b814dfbdb011f
dkkim0122 <dkkim0122@gmail.com>
2021-11-21 19:32:14 +0900

    14888 solved, using recursion

--- baekjoon/14888/14888.py
+++ baekjoon/14888/14888.py
@@ -2,19 +2,23 @@ import sys
 
 input= sys.stdin.readline
 
-def dfs(idx_op, total, plus, minus, multi, divide):
+def dfs(idx_op, total, plus, minus, multi, divide): # 1,0,1,0이 들어왔으면은
     if idx_op == n:
         result.append(total)
-        return 
+        return # 리턴값이 없으므로 다 끝나면 재귀 밖으로 리턴값 주지 않고 여기서 끝난다.
     
+    # elif로 연결된 게 아니라 if로 연결되어있어서 이런 재귀를 짤 수 있다.
     if plus:
-        dfs(idx_op+1, total + nums[idx_op], plus-1, minus, multi, divide)
+        dfs(idx_op+1, total + nums[idx_op], plus-1, minus, multi, divide) # 이 쪽에서 1,0,1,0 -> 0,0,1,0으로 재귀 한 번
     if minus:
-        dfs(idx_op+1, total - nums[idx_op], plus, minus-1, multi, divide)
+        dfs(idx_op+1, total - nums[idx_op], plus, minus-1, multi, divide) # 이 쪽에서는 안 하고
     if multi:
-        dfs(idx_op+1, total * nums[idx_op], plus, minus, multi-1, divide)
-    if divide:
-        dfs(idx_op+1, total / nums[idx_op], plus, minus, multi, divide-1)
+        dfs(idx_op+1, total * nums[idx_op], plus, minus, multi-1, divide) # 이 쪽에서 1,0,1,0 -> 1,0,0,0으로 재귀 돌고
+    if divide:                                                                
+        if total >= 0:
+            dfs(idx_op+1, total // nums[idx_op], plus, minus, multi, divide-1) # 이 쪽에서도 안 함 
+        else:
+            dfs(idx_op+1, -(-total // nums[idx_op]), plus, minus, multi, divide-1) 
 
     return
 
@@ -22,9 +26,6 @@ def dfs(idx_op, total, plus, minus, multi, divide):
 n = int(input().strip())
 nums = list(map(int, input().split()))
 cal = list(map(int, input().split()))
-
-print(nums)
-print(cal)
 
 result = []
 
