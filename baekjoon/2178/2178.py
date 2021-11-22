fe7ffb42cc843151bba9736ae83dd85ba5a3386c
dkkim0122 <dkkim0122@gmail.com>
2021-11-22 16:11:26 +0900

    2178 solved, how to check the number of blocks until current block?

--- baekjoon/2178/2178.py
+++ baekjoon/2178/2178.py
@@ -6,19 +6,24 @@ input = sys.stdin.readline
 
 def bfs(r, c):
     need_visit = deque([[r,c]])
+    visited[r][c] = 1
 
-    while need_visit and not (r==row-1 and c==col-1):
+    while need_visit:
         y, x = need_visit.popleft()
-        visited[y][x] = 1
+
+        if y == row-1 and x == col-1:
+            return visited[y][x]      
 
         for i in range(4):
             new_x = x+dx[i]
             new_y = y+dy[i]
 
-            if 0<=new_x<col and 0<=new_y<row and maze[new_y][new_x] != 0 \
-                and visited[new_y][new_x] == 0:
-                need_visit.append([new_y, new_x])
-
+            if 0<=new_x<col and 0<=new_y<row and maze[new_y][new_x] != 0: # 길이 있다
+                if visited[new_y][new_x] == 0: # 방문한 적 없다.
+                    need_visit.append([new_y, new_x])
+                    visited[new_y][new_x] = visited[y][x] + 1 # 이렇게 정의하면
+                    # 갈 수 있는 가능성이 있는 길까지 걸린 칸의 개수
+                
 
 row, col = map(int, input().split())
 maze = [[] for _ in range(row)]
@@ -28,13 +33,8 @@ for i in range(row):
     for j in str:
         maze[i].append(int(j))
 
-dx = [0,1,0,-1] # 상,우,하,좌
+dx = [0,1,0,-1] # 하,우,상,좌
 dy = [1,0,-1,0]
 visited = [[0]*col for _ in range(row)]
 
-bfs(0,0)
-total = 0
-for i in range(row):
-    total += sum(visited[i])
-
-print(total)
+print(bfs(0,0))
