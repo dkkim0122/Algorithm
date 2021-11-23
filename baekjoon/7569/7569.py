f92aa7ca4fdaf080d46c977bf00a76df343f1b6c
dkkim0122 <dkkim0122@gmail.com>
2021-11-23 14:06:13 +0900

    7569 keep failed. have to find the counterexample

--- baekjoon/7569/7569.py
+++ baekjoon/7569/7569.py
@@ -1,15 +1,8 @@
 import sys
-from copy import deepcopy
 from collections import deque
 
 def bfs(z,y,x):
-    dx = [0,0,-1,1,0,0] # 상,하,좌,우,업,다운
-    dy = [-1,1,0,0,0,0]
-    dz = [0,0,0,0,-1,1]
     need_visit = deque([[z,y,x]])
-    visited = [[[0]*m for _ in range(n)] for _ in range(h)]
-    visited[z][y][x] = 1
-
     while need_visit:
         cz, cy, cx = need_visit.popleft()
         for i in range(6):
@@ -24,9 +17,14 @@ def bfs(z,y,x):
 
 if __name__=="__main__":
     input = sys.stdin.readline
-    
+
+    dx = [0,0,-1,1,0,0] # 상,하,좌,우,업,다운
+    dy = [-1,1,0,0,0,0]
+    dz = [0,0,0,0,-1,1]
+
     m, n, h = map(int, input().split())
     box = [[[] for _ in range(n)] for _ in range(h)]
+    visited = [[[0]*m for _ in range(n)] for _ in range(h)]
 
     # box 만들기
     for k in range(h):
@@ -37,25 +35,26 @@ if __name__=="__main__":
     for k in range(h):
         for j in range(n):
             for i in range(m):
-                if box[k][j][i] == 1:
+                if box[k][j][i] == 1 and visited[k][j][i] == 0: #익은 토마토
+                    visited[k][j][i] == 1
                     bfs(k,j,i)
+    # for i in box:
+    #     print(i)          
 
-    result = 0
-    no_ripe = 0
-    
     # 한번도 변하지 않은 애
+    result = 0
+    no_ripe = True
     for k in range(h):
         for j in range(n):
-            result = max(box[k][j])
             for i in range(m):
                 if box[k][j][i] == 0: 
-                    no_ripe = -1
-                    break
+                    no_ripe = False
+                result = max(box[k][j][i], result)
     
-    if no_ripe == -1:
+    if no_ripe == False:
         print(-1)
     else:
         if result == 1:
             print(0)
         else:
-            print(result)
+            print(result -1)
