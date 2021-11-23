4b5fb2da1c261578d8f6c674249da86f6cdc41df
dkkim0122 <dkkim0122@gmail.com>
2021-11-23 18:26:13 +0900

    3055 solved...

--- baekjoon/3055/3055.py
+++ baekjoon/3055/3055.py
@@ -1,28 +1,30 @@
 import sys
 from collections import deque
-from copy import deepcopy
 
+# 물의 경로
 def water():
     while next_water:
         cy, cx = next_water.popleft()
         for i in range(4):
             ny, nx = cy+dy[i], cx+dx[i]
-            if 0<=ny<r and 0<=nx<c and water_move[ny][nx]==INF:
-                if ny != beaver_y and nx != beaver_x:
-                    water_move[ny][nx] = water_move[cy][cx] + 1
-                    next_water.append([ny, nx])
+            # 물이 갈 수 있는 길
+            if 0<=ny<r and 0<=nx<c and forest[ny][nx] == '.' and water_move[ny][nx]==-1: 
+                water_move[ny][nx] = water_move[cy][cx] + 1 # 각 위치에 물이 차는 시간을 저장한다
+                next_water.append([ny, nx])
 
+# 고슴도치의 경로
 def move(start):
     start_y ,start_x = start
     next_place = deque([[start_y ,start_x]])
 
     while next_place:
         cy, cx = next_place.popleft()
-        
         for i in range(4):
             ny, nx = cy+dy[i], cx+dx[i]
-            if 0<=ny<r and 0<=nx<c and hedge_move[ny][nx] == 0:
-                if hedge_move[cy][cx] + 1 < water_move[ny][nx]:
+            # 고슴도치가 갈 수 있는 경로('.'과 'D'는 갈 수 있다)
+            if 0<=ny<r and 0<=nx<c and forest[ny][nx] in '.D' and hedge_move[ny][nx] == -1:
+                # 1.한 시간 뒤보다 더 늦게 물이 들어오면 || 2.물이 안 들어온 애들이면 고슴도치가 갈 수 있다
+                if hedge_move[cy][cx] + 1 < water_move[ny][nx] or water_move[ny][nx] == -1:
                     hedge_move[ny][nx] = hedge_move[cy][cx] + 1
                     next_place.append([ny, nx])
 
@@ -32,7 +34,6 @@ if __name__=='__main__':
 
     dx = [0,1,0,-1] # 상,우,하,좌
     dy = [-1,0,1,0]
-    INF = sys.maxsize
     r, c = map(int, input().split())
 
     forest = [[] for _ in range(r)]
@@ -40,8 +41,8 @@ if __name__=='__main__':
         string = input().split()
         forest[i] = list(string[0])
 
-    water_move = [[INF]*c for i in range(r)]
-    hedge_move = [[0]*c for i in range(r)]
+    water_move = [[-1]*c for i in range(r)]  # default 값은 -1로 해준다.
+    hedge_move = [[-1]*c for i in range(r)]
 
     next_water = deque()
     hedge_start = [0,0]
@@ -50,38 +51,23 @@ if __name__=='__main__':
     for i in range(r):
         for j in range(c):
             if forest[i][j] == '*':
-                water_move[i][j] = 1
+                water_move[i][j] = 0 # 0부터 시작
                 next_water.append([i,j])
             elif forest[i][j] == 'S':
-                hedge_move[i][j] = 1
+                hedge_move[i][j] = 0 # 0부터 시작
                 hedge_start = [i,j]
             elif forest[i][j] == 'D':
                 beaver = [i,j] # 비버 집이 여기 있다
-            elif forest[i][j] == 'X':
-                water_move[i][j] = -1 # -1로 하면 나중에 고슴도치도 피해간다
 
+    # 비버 집 좌표
     beaver_y = beaver[0]
     beaver_x = beaver[1]
-    # print(beaver)
 
-    water()
-    # print(water_move)   
-
-    move(hedge_start)
-    # print(hedge_move)
-    # print(f'water move = {water_move}') 
-    # print(f'hedge_move = {hedge_move}')
-
-
-    # max_time = 0
-    # for row in hedge_move:
-    #     for point in row:
-    #         max_time = max(max_time, point)
+    water() # 물이 흐르고
+    move(hedge_start) # 고슴도치가 이동
 
     hedge_end = hedge_move[beaver_y][beaver_x]
-    if  hedge_end != INF: # 비버 집에 잘 도착했다
-        print(hedge_end-1)
+    if  hedge_end != -1 : # 비버 집에 잘 도착했다
+        print(hedge_end)
     else:
         print('KAKTUS')
-
-
