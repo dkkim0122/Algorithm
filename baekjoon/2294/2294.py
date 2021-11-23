47b4b44515b87ee5bd9287b14163343e8c7d2e28
dkkim0122 <dkkim0122@gmail.com>
2021-11-23 21:52:05 +0900

    2294 solved. using bfs. rememeber if you managed to reach the target value at one bfs level, that level is the minimum

--- baekjoon/2294/2294.py
+++ baekjoon/2294/2294.py
@@ -1,38 +1,35 @@
 import sys
 from collections import deque
 
-def bsf(start, target):
-    need_cal = deque([start])
-    count = 0
-    while need_cal:
-        coin = need_cal.popleft()
-        target -= coin
-        count += 1
-        if target == 0:
-            result.append(count)
-            return
-        for another_coin in coins:
-            if target - another_coin > 0:
-                need_cal.append(another_coin)
-            elif target - another_coin == 0:
-                count += 1
-                result.append(count)
+def bfs():
+    while need_visit:
+        sum, count = need_visit.popleft()
+        flags[sum] = 1
+        for coin in coins:
+            new_sum = coin + sum
+            if new_sum <= target and flags[new_sum] == 0:
+                flags[new_sum] = 1
+                new_count = count + 1 # 다음 레벨이 된다. 만약 이 레벨에서 끝나면 count는 최솟값이 되겠지
+                need_visit.append((new_sum, new_count))
+                if new_sum == target:
+                    return new_count
+    return -1
 
 
 if __name__=='__main__':
     input = sys.stdin.readline
 
     n, target = map(int, input().split())
-    coins = [int(input().strip()) for _ in range(n)]
+
+    flags = [0]*(target+1) # 해당 숫자가 합산에 쓰였는지 알 수 있도록
+    coins = []
+    for i in range(n):
+        coin = int(input().strip())
+        if coin <= target:
+            coins.append(coin)
+
     coins.sort(reverse=True)
 
-    result = []
-
-    for coin in coins:
-        if target - coin >= 0:
-            bsf(coin, target)
-
-    if result != []:
-        print(min(result))
-    else:
-        print(-1)
+    need_visit = deque()  # 합산된 값, 합산 횟수
+    need_visit.append((0,0))
+    print(bfs())
