99697b558e875e115f1acc78f42e70bd8f9bd50c
dkkim0122 <dkkim0122@gmail.com>
2021-11-24 14:06:09 +0900

    2637 solved, using topology sort, making more simple

--- baekjoon/2637/2637.py
+++ baekjoon/2637/2637.py
@@ -5,14 +5,19 @@ import sys
 from collections import deque
 
 def topology_sort():
+    queue = deque()
+    for i in range(1, node_num+1):
+        if indegree[i] == 0:
+            queue.append(i)
+    
     while queue:
         node = queue.popleft()
-        for adj in graph[node]:
-            start, end, cost = adj  # graph[start] = (start, end, cost)
-            costs_list[end] += costs_list[node]*cost  # cost 계산
-            indegree[end] -= 1
-            if indegree[end] == 0:
-                queue.append(end)
+        for adj, cost in graph[node]: # graph[start] = (adj, cost)
+            val = 1 if costs_list[node] == 0 else costs_list[node] # 완제품의 cost를 1로 잡고
+            costs_list[adj] += val * cost  # cost 계산
+            indegree[adj] -= 1
+            if indegree[adj] == 0:
+                queue.append(adj)
 
 
 if __name__=='__main__':
@@ -23,20 +28,13 @@ if __name__=='__main__':
 
     graph = [[] for _ in range(node_num + 1)]
     indegree = [0] * (node_num + 1)
-    costs_list = [0] * (node_num + 1)
-    costs_list[node_num] = 1 # 완제품의 cost를 1로 잡고 시작
+    costs_list = [0] * (node_num + 1)  # 각 노드 값이 사용된 횟수
 
     for i in range(edge_num):
         start, end, num = map(int,input().split())
-        graph[start].append((start, end, num))
+        graph[start].append((end, num))
         indegree[end]+=1
 
-    queue = deque()
-    for i in range(1, node_num+1):
-        if indegree[i] == 0:
-            queue.append(i)
-
-    # print(queue)
     topology_sort()
 
     for i in range(1,node_num+1):  # 그래프에서 indegree가 0인, 즉 기본 부품들
