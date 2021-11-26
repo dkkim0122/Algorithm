4263c2dfb966b7b1d330f1974b6a59914fdf90e8
dkkim0122 <dkkim0122@gmail.com>
2021-11-26 22:59:48 +0900

    12865 solved, dp, bottom-up

--- baekjoon/12865/12865.py
+++ baekjoon/12865/12865.py
@@ -1,23 +1,32 @@
 import sys
 input = sys.stdin.readline
 
-def find_max_value():
-    for i in range(limit_weight+1):
-        for weight in items:
-            if (i-weight) in items and weight != (i-weight):
-                dp[i] = max(dp[i], dp[weight]+dp[i-weight])
-    print(dp)
-    return dp[limit_weight]
+# dp[i][j]의 의미는, 1) 최대 가방 무게가 j일 때
+# 2) i번째 물건이 가방에 있을 수도 있고, 없을 수도 있을 때 그 둘 중 더 큰 가치의 값을 나타낸다.
+# i번째 물건이 가방에 없다면 
+# a) [(i-1개의 물건이 가방에 있고 최대 무게가 j에서 i번째 물건의 무게를 뺀 값의 가치) + (i번째 물건의 가치)]와
+# b) [i-1개의 물건이 가방에 있고 최대 무게가 그대로 j]인 두 값 중 큰 것의 값과 같다.
+# 즉 dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]]+value[i]).
+def knap_sack_recur(weight, value):
+    dp = [[0] * (limit_weight+1) for _ in range(num+1)]
+    for i in range(1, num+1):
+        for j in range(1, limit_weight+1):
+            if j >= weight[i]:
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]] + value[i])
+            else:
+                dp[i][j] = dp[i-1][j]
 
+    return dp[num][limit_weight]
 
+    
 if __name__=='__main__':
     num, limit_weight = map(int, input().split())
-    dp = [0] * (limit_weight+1)  # 각 idx가 무게, 값이 그 무게에서의 최대 가치
-    items = []
+    weight = [0]*(num+1)
+    value = [0]*(num+1)
 
-    for i in range(num):
-        weight, value = map(int, input().split())            
-        dp[weight] = value
-        items.append(weight)
+    for i in range(1, num+1):
+        w, v = map(int, input().split())            
+        weight[i] = w
+        value[i] = v
 
-    print(find_max_value())
+    print(knap_sack_recur(weight, value))
