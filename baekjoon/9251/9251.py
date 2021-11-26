6f500e83c77739838c0c503bba7b674b96d4f7c1
dkkim0122 <dkkim0122@gmail.com>
2021-11-26 19:26:15 +0900

    9251 solved, dp, top-down

--- baekjoon/9251/9251.py
+++ baekjoon/9251/9251.py
@@ -1,27 +1,31 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**8)
 
-def find_lcs(str1, str2):
-    row = len(str1)
-    col = len(str2)
+def find_lcs_recur(n, m):
 
-    dp = [[0]*(col+1) for _ in range(row+1)]
+    if n==0 or m==0: # lcs(0,...), lcs(...,0)의 값은 0이다.
+        return 0
 
-    for i in range(0, row+1):
-        for j in range(0, col+1):
-            if i == 0 or j == 0:
-                dp[i][j] = 0
-            elif str1[i-1] == str2[j-1]:
-                dp[i][j] = dp[i-1][j-1] + 1
-            else:
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    # matrix의 row: len(str1), col: len(str2)이므로, 0번째 인덱스가 0이 아니다.
+    # lcs(n, m)의 값을 matrix[n-1][m-1]에서 찾는다.
+    if dp[n-1][m-1] != -1:
+        return dp[n-1][m-1]
 
-    # for i in dp:
-    #     print(i)
-    return dp[row][col]
+    str1_final_char = str1[n-1]
+    str2_final_char = str2[m-1]
+
+    if str1_final_char==str2_final_char:
+        dp[n-1][m-1] = find_lcs_recur(n-1, m-1) + 1 # dp[n-1][m-1]의 값은 dp[n-2][m-2]에서.
+    else:
+        dp[n-1][m-1] = max(find_lcs_recur(n-1, m), find_lcs_recur(n, m-1))
+
+    return dp[n-1][m-1]
 
 if __name__=='__main__':
     str1 = input().strip()
     str2 = input().strip()
 
-    print(find_lcs(str1, str2))
+    dp = [[-1]*(len(str2)+1) for _ in range(len(str1)+1)] # default값을 -1로 둔다.
+
+    print(find_lcs_recur(len(str1), len(str2)))
