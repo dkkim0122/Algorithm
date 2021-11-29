775c1c03c1514dda9dc5dc5bfa2dd72bcc553da9
dkkim0122 <dkkim0122@gmail.com>
2021-11-29 21:35:45 +0900

    1783 solved, search every possible cases

--- baekjoon/1783/1783.py
+++ baekjoon/1783/1783.py
@@ -5,44 +5,20 @@ input = sys.stdin.readline
 
 
 if __name__=='__main__':
-    row, col = map(int, input().split())
-    used = [False]*4
-    visited = [[[0,used]]*col for _ in range(row)]
- 
-    dx = [1,1,2,2]
-    dy = [-2,2,-1,1]
+    Y, X = map(int, input().split())
 
-    sick_knight = [row-1, 0]
-    x = sick_knight[1]
-    y = sick_knight[0]
-    visited[y][x][0] = 1
+    squares = 0
 
-    # if col<=4: # 이동 방법 상관 없음
-    #     queue = deque()
-    #     queue.append(sick_knight)
-    #     while queue:
-    #         cy, cx = queue.popleft()
-    #         for i in range(4):
-    #             nx = cx+dx[i]
-    #             ny = cy+dy[i]
-    #             if 0<=nx<col and 0<=ny<row and visited[ny][nx] == 0:
-    #                 visited[ny][nx] = visited[cy][cx][0] + 1
-    #                 queue.append([ny,nx])
-    # else:
-        
-    queue = deque()
-    queue.append([y,x])
-    while queue:
-        cy, cx = queue.popleft()
-        for i in range(4):
-            nx = cx+dx[i]
-            ny = cy+dy[i]
-            if 0<=nx<col and 0<=ny<row and visited[ny][nx][0] == 0:
-                visited[ny][nx][0] = visited[cy][cx][0] + 1
-                visited[ny][nx][1][i] = True            
-                queue.append([ny,nx])
+    if Y == 1:
+        squares = 1
+    elif Y == 2:
+        squares = min(4, (X+1)//2)
+    elif Y >= 3:
+        if X <= 4:
+            squares = X
+        elif X==5 or X==6:
+            squares = 4
+        elif X >= 7:
+            squares = X-7+5
     
-
-    for i in visited:
-        print(i)
-
+    print(squares)
