c6f264d26d36fd97a3c50fef7bda5b35d2b2ed72
dkkim0122 <dkkim0122@gmail.com>
2021-11-30 14:29:18 +0900

    2579 solved, using dp

--- baekjoon/2579/2579.py
+++ baekjoon/2579/2579.py
@@ -3,11 +3,15 @@ input = sys.stdin.readline
 
 def find_max_path():
 
-    dp[0] = stair_cost[0]
-    dp[1] = max(0, dp[0]) + stair_cost[1]
-    dp[2] = max(stair_cost[0], stair_cost[1]) + stair_cost[2]
-    for i in range(3, N):
-        dp[i] = max(dp[i-3] + stair_cost[i-1], dp[i-2]) + stair_cost[i]
+    for i in range(N):
+        if i == 0:
+            dp[i] = stair_cost[i]
+        elif i == 1:
+            dp[i] = max(0, stair_cost[i-1]) + stair_cost[i]
+        elif i == 2:
+            dp[i] = max(stair_cost[0], stair_cost[1]) + stair_cost[i]
+        else:
+            dp[i] = max(dp[i-3] + stair_cost[i-1], dp[i-2]) + stair_cost[i]
 
     print(dp[N-1])
 
