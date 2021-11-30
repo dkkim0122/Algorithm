a54d72acc91053d2c395f06c094da6e56ef7e012
dkkim0122 <dkkim0122@gmail.com>
2021-11-30 10:45:03 +0900

    1149 solved, more simple way

--- baekjoon/1149/1149.py
+++ baekjoon/1149/1149.py
@@ -1,21 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-
-def find_min_cost():
-    # 첫 번째 집은 costs 그대로
-    # 두번째부터 R을 선택했으면, i-1번째 집에 B, G 중 최소가 되는 값을 가져와 costs[i][j]를 더한다.
-
-    for i in range(num_house):
-        if i == 0:
-            dp[i] = costs[0]
-
-        for j in range(3):
-            dp[i][j] = min(dp[i-1][(j+1)%3]+costs[i][j], dp[i-1][(j-1)%3]+costs[i][j])
-
-    return min(dp[num_house - 1])
-
-
 if __name__=='__main__':
     num_house = int(input().strip())
 
@@ -24,4 +9,11 @@ if __name__=='__main__':
     # dp[i][j] == i번째 집이 j색(R:0, G:1, B:2)을 고를 때, 그때까지의 최소 비용
     dp = [[0]*3 for _ in range(num_house)]
 
-    print(find_min_cost())
+    dp[0] = costs[0]
+    for i in range(1, num_house):
+        dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + costs[i][0]
+        dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + costs[i][1]
+        dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + costs[i][2]
+
+    print(min(dp[num_house-1][0], dp[num_house-1][1], dp[num_house-1][2]))
+
