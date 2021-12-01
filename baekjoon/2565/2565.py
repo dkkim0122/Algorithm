d482079a025ec0bb595dfa547557610594683833
dkkim0122 <dkkim0122@gmail.com>
2021-12-01 23:14:41 +0900

    2565 solved, using LIS

--- baekjoon/2565/2565.py
+++ baekjoon/2565/2565.py
@@ -1,39 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-def check(lines):
-    for i in range(len(lines)):
-        for j in range(i,len(lines)):
-            if lines[i][1] > lines[j][1]:
-                dp[i] += 1
-                dp[j] += 1
-    return dp
+def check_lis(lst):
+    dp = [1] * len(lst)
+
+    for i, num in enumerate(lst):
+        for j in range(0, i):
+            if num > lst[j]:
+                dp[i] = max(dp[i], dp[j]+1)
+    
+    return max(dp)
+
 
 if __name__ == '__main__':
     N = int(input().strip())
     lines = [list(map(int, input().split())) for _ in range(N)]
-
-
     lines.sort()
 
-    cnt = 0
+    b_nums = []
+    for a, b in lines:
+        b_nums.append(b)
 
-    while True:
-        dp = [0] * N
-
-        result = check(lines)
-        
-        if result.count(0) == N:
-            break
-
-        max = 0
-        max_idx = 0
-        for idx, num in enumerate(result):
-            if num > max:
-                max = num
-                max_idx = idx
-
-        del lines[max_idx]
-        cnt += 1
-
-    print(cnt)
+    print(N-check_lis(b_nums))
