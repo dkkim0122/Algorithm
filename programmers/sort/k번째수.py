267ca36b78d67f1023541233640513ae02e4b381
dkkim0122 <dkkim0122@gmail.com>
2021-12-06 10:34:28 +0900

    programmers k번째수 성공, 더 짧은 풀이(map, lambda)

--- programmers/sort/k번째수.py
+++ programmers/sort/k번째수.py
@@ -1,2 +1,2 @@
 def solution(array, commands):
-    return [sorted(array[start-1:end])[target-1] for start, end, target in commands]
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
