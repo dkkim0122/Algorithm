1f4fc60ad77dccf992a5565d2514eb8b67bbe3ef
dkkim0122 <dkkim0122@gmail.com>
2021-12-06 10:29:54 +0900

    programmers k번째수 성공, 더 짧은 풀이(for문)

--- programmers/sort/k번째수.py
+++ programmers/sort/k번째수.py
@@ -1,10 +1,2 @@
 def solution(array, commands):
-    answer = []
-    
-    for start, end, target in commands:
-        arr = array[start-1:end]
-        arr.sort()
-        ans = arr[target-1]
-        answer.append(ans)  
-    
-    return answer
+    return [sorted(array[start-1:end])[target-1] for start, end, target in commands]
