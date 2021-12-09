6461104c7fc150ea83ee873b282131d09ae56536
dkkim0122 <dkkim0122@gmail.com>
2021-12-09 20:53:32 +0900

    모의고사 수정. 한 문제도 맞춘 사람이 없을 때는 고려하지 않아도 된다.

--- programmers/모의고사.py
+++ programmers/모의고사.py
@@ -10,17 +10,12 @@ def solution(answers):
     students = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
     scores = [0] * 3
     result = []
-    max_score = 0
     
     for i, student in enumerate(students):
         scores[i] = check_ans(student, answers)
 
-    max_score = max(scores)
-    if max_score == 0:
-        return []
-    else:
-        for i, score in enumerate(scores):
-            if score == max_score:
-                result.append(i+1)
-        
+    for i, score in enumerate(scores):
+        if score == max(scores):
+            result.append(i+1)
+
     return result
