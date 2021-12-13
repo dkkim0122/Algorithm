9ca2228d8dfc6f5d4f0769cb3b330eaf1da7d8ce
dkkim0122 <dkkim0122@gmail.com>
2021-12-13 15:35:48 +0900

    카펫 성공. brown의 값 고려

--- programmers/exhaustive_search/카펫.py
+++ programmers/exhaustive_search/카펫.py
@@ -1,9 +1,12 @@
 def solution(brown, yellow):
-    total = brown + yellow
-    max = 0
+    lst = []
+    result = []
+    row = 0
+    sqr = int(yellow**0.5)
 
-    for i in range(2, int(total**0.5)+1):
-        if total % i == 0 and i > max:
-            max = i
-
-    return [total // max, max]
+    for i in range(1, sqr+1):
+        if yellow % i == 0:
+            y_row = max(i, yellow//i)
+            y_col = min(i, yellow//i)
+            if brown == ((y_row + y_col)*2 + 4):
+                return (y_row + 2, y_col + 2)
