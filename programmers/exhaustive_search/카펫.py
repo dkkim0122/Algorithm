621bd8ccbe5b999cf7b220688dd0197591a3b167
dkkim0122 <dkkim0122@gmail.com>
2021-12-13 15:37:36 +0900

    카펫, delete unneccesary variables

--- programmers/exhaustive_search/카펫.py
+++ programmers/exhaustive_search/카펫.py
@@ -1,12 +1,10 @@
 def solution(brown, yellow):
-    lst = []
-    result = []
-    row = 0
     sqr = int(yellow**0.5)
 
     for i in range(1, sqr+1):
         if yellow % i == 0:
             y_row = max(i, yellow//i)
             y_col = min(i, yellow//i)
+            
             if brown == ((y_row + y_col)*2 + 4):
                 return (y_row + 2, y_col + 2)
