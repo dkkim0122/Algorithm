75a0fd035214864c2afbd076765df7cadb11ccff
dkkim0122 <dkkim0122@gmail.com>
2021-12-14 13:39:45 +0900

    기능개발 성공. Double while loop doesnt seem good

--- programmers/stack_and_queue/기능개발.py
+++ programmers/stack_and_queue/기능개발.py
@@ -1,20 +1,19 @@
-from collections import deque
-
-progresses = [93, 30, 55]
-speeds = [1, 30 ,5]
-
-queue = deque(progresses)
-answer = []
-
-while queue:
-    count = 0
-    for i, func in enumerate(queue):
-        func += speeds[i]
+def solution(progresses, speeds):
+    answer = []
     
-    while queue[0] >= 100:
-        queue.popleft()
-        count += 1
-    
-    answer.append(count)
+    while progresses: # 기능이 하나도 남지 않을 때까지
+        count = 0
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
 
-print(answer)
+        # 모든 기능이 다 나가고 마지막으로 while문을 돌 때를 대비하여
+        # progresses == True라는 구문을 넣었다.
+        while progresses and progresses[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0) # 해당되는 기능이 나가면 같이 나가야 한다.
+            count += 1
+        
+        if count != 0:
+            answer.append(count)
+        
+    return answer
