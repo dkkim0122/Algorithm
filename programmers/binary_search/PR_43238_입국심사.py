9ead2cffbf825cfc2d72691deccf5327c24d5187
dkkim0122 <dkkim0122@gmail.com>
2021-12-23 16:27:09 +0900

    입국심사 failed, returned answer immediately when count == n

--- programmers/binary_search/PR_43238_입국심사.py
+++ programmers/binary_search/PR_43238_입국심사.py
@@ -3,23 +3,25 @@ import sys
 def solution(n, times):
     MAX = sys.maxsize
     
-    lst = []
-    
-    start = 0
+    # mid : 총 걸린 시간.
+    start = 1
     end = MAX
-    ret = MAX
+    answer = 0
     
     while start <= end:
         count = 0
         mid = (start + end) // 2
         for time in times:
-            if mid % time == 0:
-                count += mid // time
-        if count == n:
-            return mid
-        elif count < n:
-            end = mid - 1
-        else:
+            count += mid // time
+
+        if count < n:
             start = mid + 1
+        elif count >= n:
+            if count == n:
+                return mid
+            else:
+                end = mid - 1
+
+    return answer
 
 print(solution(6, [7,10]))
