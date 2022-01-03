ebdcf53e000f0ef670cfd7d9d1483c8aa7979f7a
dkkim0122 <dkkim0122@gmail.com>
2022-01-03 10:17:24 +0900

    순위 failed, using list and extend

--- programmers/graph/PR_49191_순위.py
+++ programmers/graph/PR_49191_순위.py
@@ -1,26 +1,33 @@
 from collections import defaultdict
 
 def solution(n, results):
-    win = defaultdict(set)  # 해당 노드가 이긴 노드
-    lose = defaultdict(set)  # 해당 노드가 진 노드
+    win = defaultdict(list)  # 해당 노드가 이긴 노드
+    lose = defaultdict(list)  # 해당 노드가 진 노드
 
     for winner, loser in results:
-        win[winner].add(loser)
-        lose[loser].add(winner)
+        win[winner].append(loser)
+        lose[loser].append(winner)
     
-    for i in range(n):
+    for i in range(1, n+1):
         for loser in win[i]:
-            win[i] |= win[loser]
+            win[i].extend(win[loser])
+            win[i] = list(set(win[i]))
+    for i in range(1, n+1):            
         for winner in lose[i]:
-            lose[i] |= lose[winner]
+            lose[i].extend(lose[winner])
+            lose[i] = list(set(lose[i]))
 
-    print(win)
-    print(lose)
+    count = 0
+    for i in range(1, n+1):
+        if len(win[i]) + len(lose[i]) == n-1:
+            count += 1
+
+    return count
 
 
-    answer = 0
-    return answer
+
+
 
 n = 5
-results = [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]
-solution(n, results)
+results = [[1, 2], [4, 5], [3, 4], [2, 3]]
+print(solution(n, results))
