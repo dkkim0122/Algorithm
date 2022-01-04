1e2f9d8396352b9bbba898e2ad960848aa5790b3
dkkim0122 <dkkim0122@gmail.com>
2022-01-04 13:20:02 +0900

    N으로표현 failed.

---
+++ programmers/dp/PR_42895_N으로표현.py
@@ -0,0 +1,39 @@
+def solution(N, number):
+    
+    dp = [[] for _ in range(8)]
+    
+    n = 1
+    while (n < 8):
+        if n == 1:
+            dp[n] = [N]
+            if N == number:
+                return n
+        
+        for i in range(2, n):
+            for j in range(1, i):
+                print(i, j, i-j)
+                Cal(dp[i], dp[j], dp[i-j])
+                dp[i] = list(set(dp[i]))
+            if number in dp[i]:
+                return i
+        n += 1
+        
+    return -1
+
+
+def cal(n, m):
+    length = len(str(m))
+    if m == 0:
+        return [n*(10**length)+m, n+m, n-m, n*m]
+    else:
+        return [n*(10**length)+m, n+m, n-m, n*m, n//m]
+
+def Cal(dp, dp1, dp2):
+    for i in range(len(dp1)):
+        for j in range(len(dp2)):
+            dp.extend(cal(dp1[i], dp2[j]))
+
+N = 5
+number = 26
+
+print(solution(N, number))
