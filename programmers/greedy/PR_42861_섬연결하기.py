438cf4a9fa7289ae8e25b58184efab1658350edf
dkkim0122 <dkkim0122@gmail.com>
2022-01-13 13:23:41 +0900

    섬 연결하기 성공, kruskal algorithm을 사용한 풀이

--- programmers/greedy/PR_42861_섬연결하기.py
+++ programmers/greedy/PR_42861_섬연결하기.py
@@ -1,45 +1,27 @@
-from collections import defaultdict
-import sys, heapq
-
 def solution(n, costs):
-    graph = defaultdict(list)
-    for land1, land2, cost in costs:
-        graph[land1].append([cost, land2])
-        graph[land2].append([cost, land1])
+    parent = [i for i in range(n)]
+
+    def find(island):
+        if parent[island] != island:
+            parent[island] = find(parent[island])
+        return parent[island]
     
-    max_list = []
-    for i in range(n):
-        result = dijkstra(i, graph, n)
-        if result != 0:
-            max_list.append(result)
+    def union(island1, island2):
+        root1 = find(island1)
+        root2 = find(island2)
 
-    return min(max_list)
+        parent[root1] = root2
+    
+    costs.sort(key = lambda x:x[2])
+    result = 0
 
-def dijkstra(start, graph, n):
-    INF = sys.maxsize
-    cost_list = [[INF, 0] for _ in range(n)]
-    heap = []
+    for road in costs:
+        island1, island2, cost = road
+        if find(island1) != find(island2):
+            union(island1, island2)
+            result += cost
 
-    cost_list[start] = [0, 1]
-    heapq.heappush(heap, (cost_list[start], start, 1))
-
-    while heap:
-        cur_cost, cur, count = heapq.heappop(heap)
-        if cost_list[cur][0] < cur_cost[0]:
-            continue
-
-        cost_list[cur][0] = cur_cost[0]
-        for adj_cost, adj in graph[cur]:
-            new_cost = cur_cost[0] + adj_cost
-            if new_cost < cost_list[adj][0]:
-                cost_list[adj] = [new_cost, count+1]
-                heapq.heappush(heap, (cost_list[adj], adj, count+1))
-    max = 0
-    print(cost_list)
-    for cost, count in cost_list:
-        if count == n and cost > max:
-            max = cost
-    return max
+    return result    
 
 n = 4
 costs = [[0, 1, 5], [1, 2, 3], [2, 3, 3], [1, 3, 2], [0, 3, 4]]
