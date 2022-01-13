ec7fb787c9cde3b73fd892399e0192a036575842
dkkim0122 <dkkim0122@gmail.com>
2022-01-13 11:51:53 +0900

    섬 연결하기 실패. 다익스크라 알고리즘을 사용

--- programmers/greedy/PR_42861_섬연결하기.py
+++ programmers/greedy/PR_42861_섬연결하기.py
@@ -9,34 +9,39 @@ def solution(n, costs):
     
     max_list = []
     for i in range(n):
-        max_list.append(dijkstra(i, graph, n))
-    
-    return max(max_list)
+        result = dijkstra(i, graph, n)
+        if result != 0:
+            max_list.append(result)
+
+    return min(max_list)
 
 def dijkstra(start, graph, n):
     INF = sys.maxsize
-    cost_list = [INF]*n
+    cost_list = [[INF, 0] for _ in range(n)]
     heap = []
 
-    cost_list[start] = 0
-    heapq.heappush(heap, (cost_list[start], start))
+    cost_list[start] = [0, 1]
+    heapq.heappush(heap, (cost_list[start], start, 1))
 
     while heap:
-        cur_cost, cur = heapq.heappop(heap)
-
-        if cost_list[cur] < cur_cost:
+        cur_cost, cur, count = heapq.heappop(heap)
+        if cost_list[cur][0] < cur_cost[0]:
             continue
 
-        cost_list[cur] = cur_cost
+        cost_list[cur][0] = cur_cost[0]
         for adj_cost, adj in graph[cur]:
-            new_cost = cur_cost + adj_cost
-            if new_cost < cost_list[adj]:
-                cost_list[adj] = new_cost
-                heapq.heappush(heap, (cost_list[adj], adj))
-    
-    return max(cost_list)
+            new_cost = cur_cost[0] + adj_cost
+            if new_cost < cost_list[adj][0]:
+                cost_list[adj] = [new_cost, count+1]
+                heapq.heappush(heap, (cost_list[adj], adj, count+1))
+    max = 0
+    print(cost_list)
+    for cost, count in cost_list:
+        if count == n and cost > max:
+            max = cost
+    return max
 
 n = 4
-costs = [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]
+costs = [[0, 1, 5], [1, 2, 3], [2, 3, 3], [1, 3, 2], [0, 3, 4]]
 
 print(solution(n, costs))
