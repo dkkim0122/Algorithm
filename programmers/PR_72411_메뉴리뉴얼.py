dccf7ca44eaeed33c761df7badfa4f37e9b715dd
dkkim0122 <dkkim0122@gmail.com>
2022-01-20 13:27:56 +0900

    메뉴 리뉴얼 성공. counter 모듈을 사용한 풀이. 딕셔너리를 사용할 필요 X

--- programmers/PR_72411_메뉴리뉴얼.py
+++ programmers/PR_72411_메뉴리뉴얼.py
@@ -1,38 +1,23 @@
-from collections import defaultdict
+from collections import Counter
 from itertools import combinations
 
 def solution(orders, course):
-    menu = defaultdict(int)
-    orders_set = []
     answer = []
 
-    for order in orders:
-        orders_set.append(set(order))
-
-    length = len(orders_set)
-    for i, order in enumerate(orders_set):
-        for j in range(i+1, length):
-            print(f'--------order = {order}, oders_set = {orders_set[j]}')
-            common = list(order & orders_set[j])
-            if common:
-                for i in range(2, len(common)+1):
-                    comb = list(combinations(common, i))
-                    for com in comb:
-                        menu[''.join(com)] += 1
-                        print(f'common = {common}, com = {com}')
+    for i in course:
+        temp = []
+        for order in orders:
+            comb = combinations(sorted(order), i)
+            temp += comb
+        counter = Counter(temp)
+        if counter and max(counter.values()) > 1:
+            for com, value in counter.items():
+                if value == max(counter.values()):
+                    answer.append(''.join(com))
             
-    print(menu)
+    return sorted(answer)
 
-    temp = [[] for _ in range(len(orders))]
-    for key, value in menu.items():
-        if len(key) in course:
-            temp[value].append(key)
-
-    print(temp)
-
-    return answer
-
-orders = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"]
-course = [2,3,5]
+orders = ["XYZ", "XWY", "WXA"]
+course = [2,3,4]
 
 print(solution(orders, course))
