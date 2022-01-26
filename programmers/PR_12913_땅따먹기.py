14768603aa3f95eee2fb23429e1c866a1ddccafe
dkkim0122 <dkkim0122@gmail.com>
2022-01-26 14:18:37 +0900

    전력망 성공, 노드의 개수를 찾는 문제

--- programmers/PR_12913_땅따먹기.py
+++ programmers/PR_12913_땅따먹기.py
@@ -3,10 +3,8 @@ def solution(land):
     dp[0] = land[0]
 
     for i in range(1, len(dp)):
-        dp[i][0] = max(dp[i-1][1], dp[i-1][2], dp[i-1][3]) + land[i][0]
-        dp[i][1] = max(dp[i-1][0], dp[i-1][2], dp[i-1][3]) + land[i][1]
-        dp[i][2] = max(dp[i-1][0], dp[i-1][1], dp[i-1][3]) + land[i][2]
-        dp[i][3] = max(dp[i-1][0], dp[i-1][1], dp[i-1][2]) + land[i][3]
+        for j in range(4):
+            dp[i][j] = max(dp[i-1][:j] + dp[i-1][j+1:]) + land[i][j]
 
     return max(dp[-1])
 
