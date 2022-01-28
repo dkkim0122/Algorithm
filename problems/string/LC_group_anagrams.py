e226af44f2ac894ed69760bee7a4ef0a0d8bb361
dkkim0122 <dkkim0122@gmail.com>
2022-01-28 19:55:09 +0900

    group anagrams shorter solution

--- problems/string/LC_group_anagrams.py
+++ problems/string/LC_group_anagrams.py
@@ -5,11 +5,9 @@ from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         dict = defaultdict(list)
-        
-        while strs:
-            string = strs.pop()
-            temp = ''.join(sorted(string))
-            dict[temp].append(string)
+
+        for word in strs:
+            dict[''.join(sorted(word))].append(word)
 
         return list(dict.values())
     
