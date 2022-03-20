dbd2315eec2a5b303bffb6abbe20b19c4d76fa0e
Dokyung Kim <dokyungkim@Dokyungui-MacBookAir.local>
2022-03-20 18:52:17 +0900

    약수의 개수와 덧셈, 제곱근까지로 범위 나눌 필요 없다.

---
+++ problems/PR_77884_약수의개수와덧셈.js
@@ -0,0 +1,27 @@
+function solution(left, right) {
+  var answer = 0;
+  
+  for (let i = left; i <= right; i++){
+      if (get_yack(i))
+          answer += i
+      else
+          answer -= i
+  }
+  
+  return answer;
+}
+
+function get_yack(num) {
+  let count = 0
+  for (let i = 1; i <= num; i++) {
+      if (num % i === 0) {
+          count++
+      }
+  }
+  if (count % 2 === 0) 
+      return true
+  else
+      return false
+}
+
+console.log(solution(4, 4))
