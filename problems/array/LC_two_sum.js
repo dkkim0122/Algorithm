ad63f3a25ca032ca6526086057f87d25cda3c871
dokyung-kim <dkkim0122@gmail.com>
2023-01-30 23:25:08 +0900

    LC_two_sum(1)

---
+++ problems/array/LC_two_sum.js
@@ -0,0 +1,12 @@
+/**
+ * O(n^2)로 해결
+*/
+var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+          if (nums[i] + nums[j] === target) {
+              return [i, j]
+          }
+      }
+  }
+};
